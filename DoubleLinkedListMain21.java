9919af0275dccb483df3340b9613ec358e05a073
shabrinaq <shabrinaqottrunnada@gmail.com>
2024-05-30 10:25:15 +0700

    first commit

--- DoubleLinkedListMain21.java
+++ DoubleLinkedListMain21.java
@@ -5,26 +5,24 @@ public class DoubleLinkedListMain21 {
 
     public static void main(String[] args)throws Exception {
         DoubleLinkedList21 dll = new DoubleLinkedList21();
+        
+        dll.print();
+        System.out.println("Size " + dll.size());
+        System.out.println("==================================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size " + dll.size());
+        System.out.println("==================================================");
+
+        dll.add(40, 1);
         dll.print();
 
+        System.out.println("Size " + dll.size());
         System.out.println("==================================================");
-        dll.addLast(50);
-        dll.addLast(40);
-        dll.addLast(10);
-        dll.addLast(20);
-        dll.print();
-        System.out.println("Size : "+dll.size());
-        System.out.println("==================================================");
-        dll.removeFirst();
-        dll.print();
-        System.out.println("Size : "+dll.size());
-        System.out.println("==================================================");
-        dll.removeLast();
-        dll.print();
-        System.out.println("Size : "+dll.size());
-        System.out.println("==================================================");
-        dll.remove(1);
-        dll.print();
-        System.out.println("Size : "+dll.size());
+        System.out.println("Data in the head of linked list is   : " + dll.getFirst());
+        System.out.println("Data in the tail of linked list is   : " + dll.getLast(0));
+        System.out.println("Data in the 1st index linked list is : " + dll.get(1));
     }
 }
